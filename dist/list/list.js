33e4070ff9f6cebdc0b3a182066a5fb0d2109f98
wolds9840 <wodls9840@gmail.com>
2026-03-21 16:41:23 +0900

    미션1 완료

---
+++ dist/list/list.js
@@ -0,0 +1,45 @@
+export function renderTodos(todos, onDone, onDelete) {
+    const todoLists = document.querySelectorAll('.todos__list');
+    const todoListElement = todoLists[0];
+    const doneListElement = todoLists[1];
+    if (!todoListElement || !doneListElement)
+        return;
+    todoListElement.innerHTML = '';
+    doneListElement.innerHTML = '';
+    todos.forEach(todo => {
+        const li = createTodoElement(todo, onDone, onDelete);
+        if (todo.isCompleted) {
+            doneListElement.appendChild(li);
+        }
+        else {
+            todoListElement.appendChild(li);
+        }
+    });
+}
+function createTodoElement(todo, onDone, onDelete) {
+    const li = document.createElement('li');
+    li.className = 'todos__item';
+    const span = document.createElement('span');
+    span.textContent = todo.text;
+    const button = document.createElement("button");
+    button.className = "todos__btn";
+    if (!todo.isCompleted) {
+        button.textContent = "완료";
+        button.style.backgroundColor = "#31975e";
+        button.style.color = "white";
+        button.addEventListener("click", () => {
+            onDone(todo.id);
+        });
+    }
+    else {
+        button.textContent = "삭제";
+        button.style.backgroundColor = "#c94c5b";
+        button.style.color = "white";
+        button.addEventListener("click", () => {
+            onDelete(todo.id);
+        });
+    }
+    li.appendChild(span);
+    li.appendChild(button);
+    return li;
+}
---
+++ src/input/input.ts
@@ -0,0 +1,30 @@
+//입력창과 버튼에 이벤트를 연결하는 함수
+//버튼에 추가할 이벤트를 받는다.
+export function setupInput(onAdd: (text: string) => void): void {
+    const inputField = document.querySelector('.input__field') as HTMLInputElement | null;
+    const addButton = document.querySelector('.input__button') as HTMLButtonElement | null;
+
+    if (!inputField || !addButton) return;
+
+    //버튼
+    //text가 있는 경우에만 onAdd + 입력창 초기화
+    addButton.addEventListener('click', () => {
+        const text = inputField.value.trim();
+        if (text) {
+            onAdd(text);
+            inputField.value = '';
+        }
+    });
+
+    //인풋
+    //enter 키를 누르면 마찬가지로 onAdd + 입력창 초기화
+    inputField.addEventListener('keypress', (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            const text = inputField.value.trim();
+            if (text) {
+                onAdd(text);
+                inputField.value = '';
+            }
+        }
+    });
+}
