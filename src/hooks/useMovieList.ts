0ccdc426468dfd64f7a0ad4cee13c8ba590d5ff9
wolds9840 <wodls9840@gmail.com>
2026-04-08 16:32:40 +0900

    포멧 수정

--- src/hooks/useMovieList.ts
+++ src/hooks/useMovieList.ts
@@ -31,8 +31,9 @@ export const useMovieList = (category: MovieListCategory) => {
     error,
   } = useCustomFetch(() => getMoviesByCategory[category](page), [category, page]);
 
-  //api는 category와 page가 변할 때만
+  //useEffect가 하는 일이 상태 등록말곤 없어서 조금 고민
   useEffect(() => {
+    //응답을 가져오는 순간 null일수도 있다네요
     if (!movieResponse) return;
     setMovies(movieResponse.results);
     setTotalPages(movieResponse.total_pages);
